7531b8f08cab46bd410aa01bd254f70af19f804d
Артём Тихонов <mishilen2002@gmail.com>
2023-12-15 12:27:42 +0300

    fix brainPrime

--- src/games/brainPrime.js
+++ src/games/brainPrime.js
@@ -6,7 +6,7 @@ const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
   for (let i = 2; i < num; i += 1) {
-    if (num % i === 0) {
+    if (num === 1 || num % i === 0) {
       return false;
     }
   }
